d0582afcaaa2f411934c5198caec61b0fa81db20
KonradJadwizyc <konradjadwizyc@gmail.com>
2018-11-19 21:58:10 +0100

    przykład gotowy

--- Clean_energy.R
+++ Clean_energy.R
@@ -4,10 +4,8 @@ library(tidyverse)
 library(dplyr)
 ## pobieram listę krajów 
 
-Clean_Water <- function() {
+Clean_energy <- function() {
   
-  # wczytanie krajów
-  Country_list()
   
   ## pobieram dane dla okreslonego kraju
   ## trzeba ustawiac argumenty zapytania przez page, pageSize, czyli to co jest opisane w Parameters
@@ -16,7 +14,7 @@ Clean_Water <- function() {
     
     
     tryCatch({ # w przypadku baraku internetu wywoła wyjątek
-  Clean_water <- 'https://unstats.un.org/SDGAPI/v1/sdg/Goal/Data?page=6&pageSize=1393'
+      energy <- fromJSON('https://unstats.un.org/SDGAPI/v1/sdg/Target/Data?target=7.1&pageSize=14531')
     }, error = function(err) {
       
       warning("You used bad link!")
@@ -28,23 +26,12 @@ Clean_Water <- function() {
     warning("You lost connection to internet!")
     
   }    
+  Goal_7 <- energy$data
+  Goal_7_df <- Goal_7
   
-  dane3 <- read_json(Clean_water, simplifyVector = T)
-  str(dane3,1)
-  Clean_water_df <- dane3$data 
+ 
+
+  return(Goal_7_df)
   
-  # oczyszczanie dzanych 
-  # dane globalne
-  Goal3 <- Clean_water_df %>% select(c(geoAreaCode,geoAreaName,timePeriodStart,value,seriesDescription)) %>%
-    group_by(timePeriodStart,geoAreaName)
-  
-  colnames(Goal3) <- c("Geo_ID",
-                       "Country",
-                       "Time",
-                       "Value",
-                       "Description")
-  
-  
-  Goal3_df <- data.frame(Goal3)
-  return(Goal3_df)
 }
+Goal_7 <- Clean_energy()
