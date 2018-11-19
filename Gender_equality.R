5a6813fbef6e1fd5ae077b64a13fcde19e620c6e
KonradJadwizyc <konradjadwizyc@gmail.com>
2018-11-19 21:58:40 +0100

    przykład gotowy

---
+++ Gender_equality.R
@@ -0,0 +1,42 @@
+## pakiety
+library(jsonlite)
+library(tidyverse)
+library(dplyr)
+## pobieram listę krajów 
+
+Gender_equality <- function() {
+  
+  
+  ## pobieram dane dla okreslonego celu
+  ## trzeba ustawiac argumenty zapytania przez page, pageSize, czyli to co jest opisane w Parameters
+  if(have_ip() == T) {
+    
+    
+    
+    tryCatch({ # w przypadku baraku internetu wywoła wyjątek
+      
+      equality <- fromJSON("https://unstats.un.org/SDGAPI/v1/sdg/Target/Data?target=5.4&pageSize=5640")
+    }, error = function(err) {
+      
+      warning("You used bad link!")
+      
+    })
+    
+  }else{
+    
+    warning("You lost connection to internet!")
+    
+  }    
+  
+  # oczyszczanie dzanych 
+  # dane globalne
+  Goal_5 <- equality$data
+  
+  Goal_5_df <- Goal_5 
+  
+  
+  return(Goal_5_df)
+  
+}
+
+Goal_5 <- Gender_equality()
